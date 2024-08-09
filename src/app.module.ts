896d1b64dfab2eddc332b1f7e173c9f30f488067
eleo romario <eleo.romario@gmail.com>
2024-08-09 11:26:40 -0500

    feat: agregar versión 1 a la ruta principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { ShopModule } from './modules/shop/shop.module';
+import { SharedModule } from './modules/shared/shared.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    PrismaModule
+    PrismaModule,
+    AdminModule,
+    ShopModule,
+    SharedModule
   ],
   controllers: [AppController],
   providers: [AppService]
