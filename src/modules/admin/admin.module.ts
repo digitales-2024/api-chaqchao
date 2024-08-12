c1a8fbcf09e601866a857b93007c38bc53f5fe1e
eleo romario <eleo.romario@gmail.com>
2024-08-12 17:23:44 -0500

    feat: agregar módulo de roles al módulo de administración

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [UsersModule, AuthModule]
+  imports: [UsersModule, AuthModule, RoleModule]
 })
 export class AdminModule {}
