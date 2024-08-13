1f9fe061c8c07ba0b63fea7daa1429b149f338e4
eleo romario <eleo.romario@gmail.com>
2024-08-13 16:58:58 -0500

    chore: agregar DTO para actualizar contraseña de usuario

---
+++ src/modules/admin/auth/dto/update-password.dto.ts
@@ -0,0 +1,32 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from '../../users/dto';
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class UpdatePasswordDto extends PartialType(CreateUserDto) {
+  @IsString()
+  @IsNotEmpty()
+  passwordTemp: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'the new password is too weak, it must contain at least one uppercase letter, one lowercase letter, one number'
+  })
+  @Transform(({ value }) => value.trim())
+  newPassword: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'the confirm password is too weak, it must contain at least one uppercase letter, one lowercase letter, one number'
+  })
+  @Transform(({ value }) => value.trim())
+  confirmPassword: string;
+}
