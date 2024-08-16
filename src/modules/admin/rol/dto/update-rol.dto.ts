52fe27b6aa13cf06473be733cf9caa7b017b5ee9
eleo romario <eleo.romario@gmail.com>
2024-08-16 13:31:11 -0500

    chore: actualizar DTO de actualización de roles

--- src/modules/admin/rol/dto/update-rol.dto.ts
+++ src/modules/admin/rol/dto/update-rol.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRolDto } from './create-rol.dto';
 
-export class UpdateRolDto extends PartialType(CreateRolDto) {}
+export class UpdateRolDto extends PartialType(CreateRolDto) {
+  name?: string;
+  description?: string;
+}
