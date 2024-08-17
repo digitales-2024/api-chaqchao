2b3c1c7cb2d64096f10912f6e4c81e6be85d1ad9
eleo romario <eleo.romario@gmail.com>
2024-08-17 12:11:24 -0500

    chore: agregar dependencias de correo electrónico y servicio de correo electrónico

---
+++ src/modules/email/email.service.ts
@@ -0,0 +1,24 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+import { EventPayloads } from 'src/interfaces/event-types.interface';
+
+@Injectable()
+export class EmailService {
+  constructor(private readonly mailerService: MailerService) {}
+
+  @OnEvent('user.welcome')
+  async welcomeEmail(data: EventPayloads['user.welcome']) {
+    const { name, email } = data;
+    const subject = `Welcome to Company: ${name}`;
+
+    try {
+      await this.mailerService.sendMail({
+        to: email,
+        subject,
+        template: 'welcome',
+        context: { name }
+      });
+    } catch (error) {}
+  }
+}
