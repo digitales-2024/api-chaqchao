21d0c738219982c17ec7199de8e1de81fe143101
eleo romario <eleo.romario@gmail.com>
2024-08-19 12:49:32 -0500

    feat: eliminar campo de nombre en DTO de envío de correo electrónico

--- src/modules/admin/users/dto/send-email.dto.ts
+++ src/modules/admin/users/dto/send-email.dto.ts
@@ -1,11 +1,7 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class SendEmailDto {
   @IsEmail()
   @IsNotEmpty()
   email: string;
-
-  @IsString()
-  @IsNotEmpty()
-  name: string;
 }
