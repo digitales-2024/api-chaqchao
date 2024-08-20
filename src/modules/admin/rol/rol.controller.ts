b5a828dbc292258ffd1eab100c0e211e8de4c9a7
eleo romario <eleo.romario@gmail.com>
2024-08-20 16:05:51 -0500

    feat: agregar decoradores de Swagger al controlador de roles

--- src/modules/admin/rol/rol.controller.ts
+++ src/modules/admin/rol/rol.controller.ts
@@ -3,15 +3,30 @@ import { RolService } from './rol.service';
 import { CreateRolDto } from './dto/create-rol.dto';
 import { Auth, GetUser } from '../auth/decorators';
 import { UpdateRolDto } from './dto/update-rol.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse
+} from '@nestjs/swagger';
 import { HttpResponse, Rol, UserData } from 'src/interfaces';
 
 @ApiTags('Rol')
+@ApiUnauthorizedResponse({ description: 'Unauthorized' })
+@ApiInternalServerErrorResponse({ description: 'Internal server error' })
+@ApiBearerAuth()
 @Controller({ path: 'rol', version: '1' })
 @Auth()
 export class RolController {
   constructor(private readonly rolService: RolService) {}
 
+  @ApiCreatedResponse({ description: 'Rol created' })
+  @ApiBadRequestResponse({ description: 'Rol already exists and schema errors' })
+  @ApiBody({ type: CreateRolDto })
   @Post()
   create(
     @Body() createRolDto: CreateRolDto,
@@ -20,6 +35,9 @@ export class RolController {
     return this.rolService.create(createRolDto, user);
   }
 
+  @ApiOkResponse({ description: 'Rol updated' })
+  @ApiBadRequestResponse({ description: 'No data to update' })
+  @ApiBody({ type: UpdateRolDto })
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -29,16 +47,22 @@ export class RolController {
     return this.rolService.update(id, updateRolDto, user);
   }
 
+  @ApiBadRequestResponse({ description: 'Rol no found' })
+  @ApiOkResponse({ description: 'Rol deleted' })
   @Delete(':id')
   remove(@Param('id') id: string): Promise<HttpResponse<Rol>> {
     return this.rolService.remove(id);
   }
 
+  @ApiBadRequestResponse({ description: 'Rols no found' })
+  @ApiOkResponse({ description: 'Rols found' })
   @Get()
   findAll(): Promise<Rol[]> {
     return this.rolService.findAll();
   }
 
+  @ApiBadRequestResponse({ description: 'Rol no found' })
+  @ApiOkResponse({ description: 'Rol found' })
   @Get(':id')
   findById(@Param('id') id: string): Promise<Rol> {
     return this.rolService.findById(id);
