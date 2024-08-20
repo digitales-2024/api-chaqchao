1004ea1c3377689577b0279c3c0b6189056df2e4
eleo romario <eleo.romario@gmail.com>
2024-08-20 09:16:43 -0500

    feat: actualizar lógica de envío de correo electrónico a usuarios

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -1,24 +1,53 @@
 import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { EventPayloads } from 'src/interfaces/event-types.interface';
+import { getFirstWord, handleException } from 'src/utils';
+
+const infoBusiness = {
+  business: 'Chaqchao',
+  url: 'https://chaqchao.com',
+  phone: '+51 999 999 998',
+  address: '1234 Street',
+  contact: 'contacto@gmail.com'
+};
 
 @Injectable()
 export class EmailService {
+  private readonly logger = new Logger(EmailService.name);
   constructor(private readonly mailerService: MailerService) {}
 
   @OnEvent('user.welcome-admin-first')
-  async welcomeEmail(data: EventPayloads['user.welcome-admin-first']) {
-    const { name, email } = data;
-    const subject = `Welcome to Company: ${name}`;
+  async welcomeEmail(data: EventPayloads['user.welcome-admin-first']): Promise<any> {
+    const { name, email, password } = data;
+    const subject = `Bienvenido a ${infoBusiness.business}: ${getFirstWord(name)}`;
 
     try {
-      await this.mailerService.sendMail({
+      const sendingEmail = await this.mailerService.sendMail({
         to: email,
         subject,
-        template: 'welcome',
-        context: { name }
+        template: 'welcome-admin-first',
+        context: {
+          name,
+          email,
+          password,
+          business: infoBusiness.business,
+          url: infoBusiness.url,
+          phone: infoBusiness.phone,
+          address: infoBusiness.address,
+          contact: infoBusiness.contact
+        }
       });
-    } catch (error) {}
+
+      // Verificar si el correo fue enviado correctamente
+      if (sendingEmail && sendingEmail.accepted.length > 0) {
+        this.logger.log(`Email sent successfully to ${email}`);
+      } else {
+        this.logger.warn(`Email was not accepted by the server for ${email}`);
+      }
+    } catch (error) {
+      this.logger.error(error);
+      handleException(error, 'Error sending email');
+    }
   }
 }
