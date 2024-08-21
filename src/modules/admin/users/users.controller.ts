fc8b5adbebe4b2dd66c934b16568e0fff6253742
eleo romario <eleo.romario@gmail.com>
2024-08-21 16:19:44 -0500

    feat: agregar api swager y orden de los endpoints de usuario

--- src/modules/admin/users/users.controller.ts
+++ src/modules/admin/users/users.controller.ts
@@ -2,13 +2,25 @@ import { Controller, Post, Body, Get, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { GetUser } from '../auth/decorators/get-user.decorator';
-import { User } from './interfaces/user.interface';
 import { Auth } from '../auth/decorators';
 import { UpdateUserDto } from './dto';
 import { SendEmailDto } from './dto/send-email.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse
+} from '@nestjs/swagger';
+import { HttpResponse, UserData, UserDataLogin } from 'src/interfaces';
 
 @ApiTags('Users')
+@ApiBearerAuth()
+@ApiUnauthorizedResponse({ description: 'Unauthorized' })
+@ApiInternalServerErrorResponse({ description: 'Internal server error' })
+@ApiBadRequestResponse({ description: 'Bad request' })
 @Controller({
   path: 'users',
   version: '1'
@@ -17,37 +29,60 @@ import { ApiTags } from '@nestjs/swagger';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
+  @ApiCreatedResponse({ description: 'User created' })
   @Post()
-  create(@Body() createUserDto: CreateUserDto, @GetUser() user: User) {
+  create(
+    @Body() createUserDto: CreateUserDto,
+    @GetUser() user: UserDataLogin
+  ): Promise<HttpResponse<UserData>> {
     return this.usersService.create(createUserDto, user);
   }
 
+  @ApiOkResponse({ description: 'User updated' })
   @Patch(':id')
-  update(@Body() updateUserDto: UpdateUserDto, @Param('id') id: string, @GetUser() user: User) {
+  update(
+    @Body() updateUserDto: UpdateUserDto,
+    @Param('id') id: string,
+    @GetUser() user: UserDataLogin
+  ) {
     return this.usersService.update(updateUserDto, id, user);
   }
 
+  @ApiOkResponse({ description: 'User deleted' })
   @Delete(':id')
-  remove(@GetUser() user: User, @Param('id') id: string) {
-    return this.usersService.remove(user, id);
+  remove(@Param('id') id: string, @GetUser() user: UserDataLogin): Promise<HttpResponse<UserData>> {
+    return this.usersService.remove(id, user);
   }
 
-  @Get('profile')
-  profile(@GetUser() user: User) {
-    return this.usersService.profile(user);
+  @ApiOkResponse({ description: 'User deactivated' })
+  @Patch('reactivate/:id')
+  reactivate(
+    @Param('id') id: string,
+    @GetUser() user: UserDataLogin
+  ): Promise<HttpResponse<UserData>> {
+    return this.usersService.reactivate(id, user);
   }
 
-  @Get('generate-password')
-  generatePassword() {
+  @ApiOkResponse({ description: 'Get all users' })
+  @Get()
+  findAll(): Promise<UserData[]> {
+    return this.usersService.findAll();
+  }
+
+  @ApiOkResponse({ description: 'Get user by id' })
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<UserData> {
+    return this.usersService.findOne(id);
+  }
+
+  @ApiOkResponse({ description: 'Get new password' })
+  @Post('generate-password')
+  generatePassword(): HttpResponse<string> {
     return this.usersService.generatePassword();
   }
 
-  @Get('send-email')
+  @ApiOkResponse({ description: 'Send email' })
+  @Post('send-email')
   sendEmail(@Body() sendEmailDto: SendEmailDto) {
     return this.usersService.sendEmail(sendEmailDto);
   }
