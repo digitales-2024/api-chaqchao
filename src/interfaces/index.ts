362835c0d636bacbb4f704ae82097755f9f2327b
eleo romario <eleo.romario@gmail.com>
2024-08-22 15:16:41 -0500

    feat: agregar interfaces de módulo y permisos

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,5 @@ export { EventPayloads } from './event-types.interface';
 export { UserPayload, UserData, UserDataLogin } from './user.type';
 export { Rol } from './rol.type';
 export { HttpResponse } from './https-response.interface';
+export { Module } from './module.interface';
+export { Permission } from './permission.interface';
---
+++ src/interfaces/permission.interface.ts
@@ -0,0 +1,5 @@
+export interface Permission {
+  cod: string;
+  name: string;
+  description: string;
+}
