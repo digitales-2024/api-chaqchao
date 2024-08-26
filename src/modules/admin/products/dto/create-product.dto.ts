0f9a6c8e8570eba55c2a3c5f42fd633a373c81e6
eleo romario <eleo.romario@gmail.com>
2024-08-26 08:36:08 -0500

    feat: agregar DTO de creación y actualización de productos

---
+++ src/modules/admin/products/dto/create-product.dto.ts
@@ -0,0 +1,24 @@
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsString, IsUrl, IsUUID } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.trim().toLowerCase())
+  name: string;
+
+  @IsString()
+  description?: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+
+  @IsString()
+  @IsUrl()
+  image: string;
+
+  @IsNotEmpty()
+  @IsUUID()
+  categoryId: number;
+}
