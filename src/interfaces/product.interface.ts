449828cb19a10908dc787cbe6659d0e156fbc390
Diego Rondón <furrtr4532@gmail.com>
2024-08-28 10:01:54 -0500

    feat: insercion del dato isavailable a la interfaz d eproducto

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -1,5 +1,8 @@
 import { Product } from '@prisma/client';
 
-export type ProductData = Pick<Product, 'id' | 'name' | 'description' | 'price' | 'image'> & {
+export type ProductData = Pick<
+  Product,
+  'id' | 'name' | 'description' | 'price' | 'image' | 'isAvailable'
+> & {
   category: { id: string; name: string };
 };
