316dd1f31838a6efe15da00bb5dc566d7f8fd07e
Diego Rondón <furrtr4532@gmail.com>
2024-08-29 12:24:36 -0500

    feat: eliminar el controlador de product variation

--- src/interfaces/product-variation.interface.ts
+++ src/interfaces/product-variation.interface.ts
@@ -3,6 +3,4 @@ import { ProductVariation } from '@prisma/client';
 export type ProductVariationData = Pick<
   ProductVariation,
   'id' | 'name' | 'description' | 'additionalPrice'
-> & {
-  product: { id: string; name: string };
-};
+>;
