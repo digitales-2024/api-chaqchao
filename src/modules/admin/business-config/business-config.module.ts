ede4164ed1ca12d00513abbb1fba991b2c939eaa
Diego Rondón <furrtr4532@gmail.com>
2024-08-29 17:02:51 -0500

    feat: importar el modulo de prisma

--- src/modules/admin/business-config/business-config.module.ts
+++ src/modules/admin/business-config/business-config.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BusinessConfigService } from './business-config.service';
 import { BusinessConfigController } from './business-config.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [BusinessConfigController],
-  providers: [BusinessConfigService]
+  providers: [BusinessConfigService],
+  imports: [PrismaModule]
 })
 export class BusinessConfigModule {}
