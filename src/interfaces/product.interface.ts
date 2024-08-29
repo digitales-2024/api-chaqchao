8d56221ee26d61e22b27131e6774f402074a313f
Diego Rondón <furrtr4532@gmail.com>
2024-08-29 12:25:19 -0500

    feat: inserción del arreglo de variaciones para la data de producto

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -1,8 +1,10 @@
 import { Product } from '@prisma/client';
+import { ProductVariationData } from './product-variation.interface';
 
 export type ProductData = Pick<
   Product,
   'id' | 'name' | 'description' | 'price' | 'image' | 'isAvailable'
 > & {
   category: { id: string; name: string };
+  variations: ProductVariationData[];
 };
