dc3c3b502c885f3c23c2fcb866d277aa06d6964e
Diego Rondón <furrtr4532@gmail.com>
2024-08-29 12:23:57 -0500

    chore: agregar migración a la base de datos

---
+++ prisma/migrations/20240829144405_update_product_variations/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isActive` on the `ProductVariation` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ProductVariation" DROP COLUMN "isActive";
