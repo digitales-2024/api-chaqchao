2bad7a1d7c73b2f8dbc8c08964359d54e0e70b30
Diego Rondón <furrtr4532@gmail.com>
2024-08-29 12:26:13 -0500

    feat: exportar el servicio de product variation

--- src/modules/admin/product-variation/product-variation.module.ts
+++ src/modules/admin/product-variation/product-variation.module.ts
@@ -1,12 +1,11 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { ProductVariationService } from './product-variation.service';
-import { ProductVariationController } from './product-variation.controller';
 import { ProductsModule } from '../products/products.module'; // Importa el ProductsModule
 import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  controllers: [ProductVariationController],
   providers: [ProductVariationService],
-  imports: [PrismaModule, forwardRef(() => ProductsModule)]
+  imports: [PrismaModule, forwardRef(() => ProductsModule)],
+  exports: [ProductVariationService]
 })
 export class ProductVariationModule {}
