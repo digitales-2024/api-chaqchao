a088263024dc19061104ba266a769d4f89f09e00
Diego Rondón <furrtr4532@gmail.com>
2024-09-03 10:39:05 -0500

    feat: eliminacion del controller de classes

--- src/modules/admin/classes/classes.module.ts
+++ src/modules/admin/classes/classes.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClassesService } from './classes.service';
-import { ClassesController } from './classes.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { BusinessConfigModule } from '../business-config/business-config.module';
 
 @Module({
-  controllers: [ClassesController],
   providers: [ClassesService],
   imports: [PrismaModule, BusinessConfigModule]
 })
