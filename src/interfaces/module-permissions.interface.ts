2f0a1cdf900564bb9339fa5b0db12feb8ff4e9cc
eleo romario <eleo.romario@gmail.com>
2024-09-03 15:30:10 -0500

    feat: agregar campos de cod y description a módulo y permisos

--- src/interfaces/module-permissions.interface.ts
+++ src/interfaces/module-permissions.interface.ts
@@ -6,10 +6,14 @@ export interface ModulePermissions {
 export interface ModulePermissionsData {
   module: {
     id: string;
+    cod: string;
     name: string;
+    description: string;
   };
   permissions: {
     id: string;
+    cod: string;
     name: string;
+    description: string;
   }[];
 }
