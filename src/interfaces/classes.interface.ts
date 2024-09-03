402810dda187928da9517143b61612b4f1afbb09
Diego Rondón <furrtr4532@gmail.com>
2024-09-03 10:24:34 -0500

    feat: creacion de la data importante para las tablas relacionadas a las clases

---
+++ src/interfaces/classes.interface.ts
@@ -0,0 +1,20 @@
+import {
+  ClassLanguage,
+  ClassPriceConfig,
+  ClassRegistrationConfig,
+  ClassSchedule
+} from '@prisma/client';
+
+export type ClassPriceConfigData = Pick<
+  ClassPriceConfig,
+  'id' | 'classTypeUser' | 'price' | 'typeCurrency'
+>;
+
+export type ClassScheduleData = Pick<ClassSchedule, 'id' | 'startTime'>;
+
+export type ClassLanguageData = Pick<ClassLanguage, 'id' | 'languageName'>;
+
+export type ClassRegistrationData = Pick<
+  ClassRegistrationConfig,
+  'id' | 'closeBeforeStartInterval' | 'finalRegistrationCloseInterval'
+>;
