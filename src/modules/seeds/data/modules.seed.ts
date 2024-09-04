1f8a3ab7b4f27001cbc02f8817892bf4eb5ba9cf
eleo romario <eleo.romario@gmail.com>
2024-09-04 16:48:30 -0500

    feat: agregar módulos y permisos iniciales al seed de datos

--- src/modules/seeds/data/modules.seed.ts
+++ src/modules/seeds/data/modules.seed.ts
@@ -2,14 +2,48 @@ import { Module } from 'src/interfaces';
 
 export const modulesSeed: Module[] = [
   {
-    cod: 'M1',
-    name: 'Module 1',
-    description: 'This is module 1'
+    name: 'Products',
+    cod: 'PRD',
+    description: 'This is the products module'
   },
   {
-    cod: 'M2',
-    name: 'Module 2',
-    description: 'This is module 2'
+    name: 'Customers',
+    cod: 'CST',
+    description: 'This is the customers module'
+  },
+  {
+    cod: 'ORD',
+    name: 'Orders',
+    description: 'This is the orders module'
+  },
+  {
+    cod: 'USR',
+    name: 'Users',
+    description: 'This is the users module'
+  },
+  {
+    cod: 'ROL',
+    name: 'Roles',
+    description: 'This is the roles module'
+  },
+  {
+    cod: 'RPT',
+    name: 'Reports',
+    description: 'This is the reports module'
+  },
+  {
+    cod: 'BNSS',
+    name: 'Business',
+    description: 'This is the business module'
+  },
+  {
+    cod: 'STK',
+    name: 'Stock',
+    description: 'This is the stock module'
+  },
+  {
+    cod: 'PRV',
+    name: 'Providers',
+    description: 'This is the providers module'
   }
-  // Agrega más objetos de módulo según sea necesario
 ];
