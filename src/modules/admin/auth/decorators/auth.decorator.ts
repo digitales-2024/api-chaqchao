7a4a641afdf7f7dfc6ec3e0529ad62c10b0d2ac8
eleo romario <eleo.romario@gmail.com>
2024-09-05 17:06:27 -0500

    feat: habilitar autenticación con token y validación de cambio de contraseña

--- src/modules/admin/auth/decorators/auth.decorator.ts
+++ src/modules/admin/auth/decorators/auth.decorator.ts
@@ -3,6 +3,5 @@ import { AuthGuard } from '@nestjs/passport';
 import { MustChangePasswordGuard } from '../guards/must-change-password-guards.guard';
 
 export function Auth() {
-  // return applyDecorators(RolProtected(...rols), UseGuards(AuthGuard('jwt'), UserRolGuard));
   return applyDecorators(UseGuards(AuthGuard('jwt')), UseGuards(MustChangePasswordGuard));
 }
