b1eeef7706b70a63b421c7c571ae5bd5f1a0c4fa
Diego Rondón <furrtr4532@gmail.com>
2024-09-09 11:55:19 -0500

    feat: reemplazar las variables por variables de entorno

--- src/modules/shop/auth/auth.controller.ts
+++ src/modules/shop/auth/auth.controller.ts
@@ -1,49 +1,51 @@
-import {
-  Controller,
-  Get /* , Post, Body, Patch, Param, Delete */,
-  Res,
-  UseGuards
-} from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, Res, Req, UseGuards, HttpStatus } from '@nestjs/common';
+import { Response, Request } from 'express';
 import { AuthService } from './auth.service';
-import { GoogleAuthGuard } from './utils/Guards';
+import { GoogleAuthGuard } from './utils/guards.utils';
+import { AuthGuard } from '@nestjs/passport';
+import { GetClient } from './decorators/get-client.decorator';
+import { ClientData } from 'src/interfaces';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
-@Controller({ path: 'auth', version: '1' })
+@Controller({ path: 'auth/client', version: '1' })
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
-  /*   @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  } */
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService
+  ) {}
 
   @Get('google/login')
   handleLogin(@Res() res: Response) {
-    const clientId = '356142009565-ulu3ffrsmocaib7uiuerqr2ulk6kbb65.apps.googleusercontent.com';
-    const redirectUri = 'http://localhost:3000/api/v1/auth/google/redirect';
+    const clientId = this.configService.get<string>('GOOGLE_CLIENT_ID');
+    const redirectUri = this.configService.get<string>('GOOGLE_REDIRECT_URI');
     const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=email%20profile&access_type=offline&prompt=consent`;
     return res.redirect(googleAuthUrl);
   }
 
-  // api/v1/google/redirect
+  @ApiOkResponse({ description: 'User authenticated successfully' })
   @Get('google/redirect')
   @UseGuards(GoogleAuthGuard)
-  handleRedirect() {
-    return { msg: 'OK' };
+  async handleRedirect(@Req() req: Request, @Res() res: Response) {
+    if (req.user) {
+      const { user } = req;
+      return res.status(HttpStatus.CREATED).json({
+        statusCode: HttpStatus.CREATED,
+        message: 'User authenticated successfully',
+        data: user
+      });
+    } else {
+      return res.status(HttpStatus.UNAUTHORIZED).json({
+        statusCode: HttpStatus.UNAUTHORIZED,
+        message: 'Authentication failed'
+      });
+    }
   }
 
-  /*   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+  @ApiOkResponse({ description: 'Client profile' })
+  @Get('profile')
+  @UseGuards(AuthGuard('client-jwt'))
+  getClientProfile(@GetClient() client: ClientData) {
+    return client;
   }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  } */
 }
--- src/modules/shop/auth/utils/googel-strategy.utils.ts
+++ src/modules/shop/auth/utils/googel-strategy.utils.ts
@@ -3,23 +3,30 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, Profile } from 'passport-google-oauth20';
 import { AuthService } from '../auth.service';
 import { ClientGoogleData } from 'src/interfaces/client.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService
+  ) {
     super({
-      clientID: '356142009565-ulu3ffrsmocaib7uiuerqr2ulk6kbb65.apps.googleusercontent.com',
-      clientSecret: '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',
-      callbackURL: 'http://localhost:3000/api/v1/auth/client/google/redirect',
+      clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
+      clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
+      callbackURL: configService.get<string>('GOOGLE_REDIRECT_URI'),
       scope: ['openid', 'email', 'profile']
     });
   }
 
+  /**
+   * Valida la información del usuario recibida de Google
+   * @param accessToken Token de acceso
+   * @param refreshToken Token de refresco
+   * @param profile Información del usuario
+   * @returns Cliente autenticado
+   */
   async validate(accessToken: string, refreshToken: string, profile: Profile): Promise<any> {
-    console.log('Access Token:', accessToken);
-    console.log('Refresh Token:', refreshToken);
-    console.log('Profile:', profile);
-
     const email = profile.emails[0].value;
     const name = profile.displayName;
     const token = refreshToken; // Usar el refreshToken recibido de Google
