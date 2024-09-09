aeac491467d26673c4ef973c3b04f394fdc2c5ec
Diego Rondón <furrtr4532@gmail.com>
2024-09-09 11:41:24 -0500

    feat: validar el token generado al loggearse un cliente con google

---
+++ src/modules/shop/auth/utils/client-jwt.utils.ts
@@ -0,0 +1,39 @@
+// client-jwt.strategy.ts
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ClientData } from 'src/interfaces';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class ClientJwtStrategy extends PassportStrategy(Strategy, 'client-jwt') {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+    });
+  }
+
+  /**
+   * Valida el token del cliente
+   * @param payload Información del token
+   * @returns Cliente autenticado
+   */
+  async validate(payload: { id: string }): Promise<ClientData> {
+    const { id } = payload;
+    const client = await this.authService.findById(id);
+
+    if (!client) {
+      throw new UnauthorizedException('Client is not active or does not exist');
+    }
+
+    const { id: clientId, name, email } = client;
+    return { id: clientId, name, email };
+  }
+}
