0e5609092a201040470c83211860a40383ddd12a
Diego Rondón <furrtr4532@gmail.com>
2024-09-09 11:41:54 -0500

    feat: agregar modulos de jwt y de google strategy

--- src/modules/shop/auth/auth.module.ts
+++ src/modules/shop/auth/auth.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { GoogleStrategy } from './utils/GoogleStrategy';
+import { GoogleStrategy } from './utils/googel-strategy.utils';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientJwtStrategy } from './utils/client-jwt.utils';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy],
-  imports: [PrismaModule]
+  providers: [AuthService, GoogleStrategy, ClientJwtStrategy],
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get('JWT_EXPIRES_IN') }
+      })
+    })
+  ]
 })
 export class AuthModule {}
