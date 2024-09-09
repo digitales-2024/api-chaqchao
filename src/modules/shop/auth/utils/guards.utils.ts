3842c1e9230ec6942b3a3415ad8d06f4fb7bfd30
Diego Rondón <furrtr4532@gmail.com>
2024-09-09 11:54:39 -0500

    chore: eliminacion del console log de la informacion del cliente

--- src/modules/shop/auth/utils/guards.utils.ts
+++ src/modules/shop/auth/utils/guards.utils.ts
@@ -5,10 +5,6 @@ import { AuthGuard } from '@nestjs/passport';
 export class GoogleAuthGuard extends AuthGuard('google') {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const canActivate = (await super.canActivate(context)) as boolean;
-
-    const request = context.switchToHttp().getRequest();
-    console.log('User from GoogleAuthGuard:', request.user);
-
     return canActivate;
   }
 }
