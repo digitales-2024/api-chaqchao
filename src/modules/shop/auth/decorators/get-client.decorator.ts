c9031059702c0d3a40860a969e01554939072369
Diego Rondón <furrtr4532@gmail.com>
2024-09-10 12:53:12 -0500

    chore: eliminancion del console log

--- src/modules/shop/auth/decorators/get-client.decorator.ts
+++ src/modules/shop/auth/decorators/get-client.decorator.ts
@@ -8,7 +8,6 @@ import { ClientData } from 'src/interfaces';
 export const GetClient = createParamDecorator((data, ctx: ExecutionContext): ClientData => {
   const request = ctx.switchToHttp().getRequest();
   const client = request.user;
-  console.log('Client from GetClient:', client);
 
   if (!client) {
     throw new InternalServerErrorException('Client not found');
