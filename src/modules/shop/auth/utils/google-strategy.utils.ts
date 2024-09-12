6a767e60c223c599b66e35e613a1113256073ed0
Diego Rondón <furrtr4532@gmail.com>
2024-09-12 13:10:17 -0500

    feat: modifciacion de la función validate user para añadirle el parametro faltante, el resquest

--- src/modules/shop/auth/utils/google-strategy.utils.ts
+++ src/modules/shop/auth/utils/google-strategy.utils.ts
@@ -4,6 +4,7 @@ import { Strategy, Profile } from 'passport-google-oauth20';
 import { AuthService } from '../auth.service';
 import { ClientGoogleData } from 'src/interfaces/client.interface';
 import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
@@ -15,18 +16,25 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
       clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
       callbackURL: configService.get<string>('GOOGLE_REDIRECT_URI'),
-      scope: ['openid', 'email', 'profile']
+      scope: ['openid', 'email', 'profile'],
+      passReqToCallback: true
     });
   }
 
   /**
    * Valida la información del usuario recibida de Google
+   * @param req Objeto de solicitud
    * @param accessToken Token de acceso
    * @param refreshToken Token de refresco
    * @param profile Información del usuario
    * @returns Cliente autenticado
    */
-  async validate(accessToken: string, refreshToken: string, profile: Profile): Promise<any> {
+  async validate(
+    req: Request,
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile
+  ): Promise<any> {
     const email = profile.emails[0].value;
     const name = profile.displayName;
     const token = refreshToken;
@@ -35,6 +43,6 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
     const clientData: ClientGoogleData = { name, email, token };
 
     // Validar el usuario usando AuthService y retornar el cliente
-    return await this.authService.validateUserGoogle(clientData);
+    return await this.authService.validateUserGoogle(clientData, req.res);
   }
 }
