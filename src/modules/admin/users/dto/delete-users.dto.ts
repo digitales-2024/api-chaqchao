60760283bf38867d57662663667bc7a4862bc794
eleo romario <eleo.romario@gmail.com>
2024-09-14 09:43:22 -0500

    feat: refactorizar DTO de eliminación de usuarios en masa

--- src/modules/admin/users/dto/delete-users.dto.ts
+++ src/modules/admin/users/dto/delete-users.dto.ts
@@ -4,6 +4,6 @@ import { IsArray, IsString } from 'class-validator';
 export class DeleteUsersDto {
   @ApiProperty({ type: [String] })
   @IsArray()
-  @IsString({ each: true }) // Asegura que cada elemento en el array sea una cadena
+  @IsString({ each: true })
   ids: string[];
 }
