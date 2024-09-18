4d7dfecd9281679f934d23413f39efef4daff902
eleo romario <eleo.romario@gmail.com>
2024-09-18 17:25:34 -0500

    feat: agregar el nuevo evento para enviar al correo con la nueva password

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -83,4 +83,38 @@ export class EmailService {
       return false; // Retorna false indicando fallo
     }
   }
+
+  @OnEvent('user.new-password')
+  async newPassword(data: EventPayloads['user.new-password']): Promise<boolean> {
+    const { name, email, password, webAdmin } = data;
+    const subject = `Bienvenido a ${infoBusiness.business}: ${getFirstWord(name)}`;
+
+    try {
+      const sendingEmail = await this.mailerService.sendMail({
+        to: email,
+        subject,
+        template: 'welcome-admin-first',
+        context: {
+          name,
+          email,
+          password,
+          webAdmin,
+          business: infoBusiness.business,
+          url: infoBusiness.url,
+          phone: infoBusiness.phone,
+          address: infoBusiness.address,
+          contact: infoBusiness.contact
+        }
+      });
+
+      if (sendingEmail) {
+        return true; // Retorna true indicando éxito
+      } else {
+        return false; // Retorna false indicando fallo
+      }
+    } catch (error) {
+      this.logger.error(error);
+      return false; // Retorna false indicando fallo
+    }
+  }
 }
