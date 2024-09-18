46fee0c85217b7155b4b38b6af39f9c9d9be836d
Alexander Cayro <alexandercayromamani@gmail.com>
2024-09-18 15:22:12 -0500

    feat: servicio de actualizar cliente desde el admin

--- src/modules/admin/client-admin/client-admin.service.ts
+++ src/modules/admin/client-admin/client-admin.service.ts
@@ -1,21 +1,26 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { ClientPayload } from 'src/interfaces';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { AuditActionType } from '@prisma/client';
+import { ClientPayload, UserData } from 'src/interfaces';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { handleException } from 'src/utils';
+import { RolService } from '../../admin/rol/rol.service';
 
 @Injectable()
 export class ClientAdminService {
   private readonly logger = new Logger(ClientAdminService.name);
-  constructor(private readonly prisma: PrismaService) {}
-
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly rolService: RolService
+  ) {}
   /**
    * Obtiene todos los clientes
    * @returns Todos los clientes
    */
   async findAll(): Promise<ClientPayload[]> {
+    //const isSuperAdmin = await this.rolService.isRolSuperAdmin(id);
     try {
       const clients = await this.prisma.client.findMany({
-        where: { isActive: true },
+        where: {},
         select: {
           id: true,
           name: true,
@@ -42,6 +47,110 @@ export class ClientAdminService {
     } catch (error) {
       this.logger.error('Error getting all clients');
       handleException(error, 'Error getting all clients');
+    }
+  }
+
+  /**
+   * Actualiza un cliente por su ID
+   * @param id ID del cliente a actualizar
+   * @param data Datos a actualizar
+   * @returns El cliente actualizado
+   */
+  async update(id: string, data: Partial<ClientPayload>, user: UserData): Promise<ClientPayload> {
+    try {
+      const updatedClient = await this.prisma.client.update({
+        where: { id },
+        data,
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          phone: true,
+          birthDate: true,
+          isGoogleAuth: true,
+          lastLogin: true,
+          isActive: true
+        }
+      });
+
+      // Crear un registro de auditoría
+      await this.prisma.audit.create({
+        data: {
+          entityId: updatedClient.id,
+          action: AuditActionType.UPDATE,
+          performedById: user.id,
+          entityType: 'Client Admin'
+        }
+      });
+
+      return updatedClient as ClientPayload;
+    } catch (error) {
+      this.logger.error('Error updating client with id ${id}');
+      handleException(error, 'Error updating client with id ${id}');
+    }
+  }
+
+  /**
+   * Alternar estado de activación de un Client Admin
+   * @param id Id del Client Admin
+   * @param user Usuario que realiza la acción
+   * @returns Client Admin actualizado
+   */
+  async toggleActivation(id: string, user: UserData): Promise<ClientPayload> {
+    try {
+      const clientAdminUpdated = await this.prisma.$transaction(async (prisma) => {
+        const clientAdminDB = await prisma.client.findUnique({
+          where: { id },
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            phone: true,
+            isActive: true
+          }
+        });
+
+        if (!clientAdminDB) {
+          throw new NotFoundException('Client Admin not found');
+        }
+
+        // Alternar el estado de activación
+        const newIsActive = !clientAdminDB.isActive;
+
+        // Actualizar el estado de activación del cliente
+        await prisma.client.update({
+          where: { id },
+          data: {
+            isActive: newIsActive
+          }
+        });
+
+        // Crear un registro de auditoría
+        await this.prisma.audit.create({
+          data: {
+            entityId: clientAdminDB.id,
+            action: AuditActionType.UPDATE,
+            performedById: user.id,
+            entityType: 'Client Admin'
+          }
+        });
+
+        return {
+          id: clientAdminDB.id,
+          name: clientAdminDB.name,
+          email: clientAdminDB.email,
+          phone: clientAdminDB.phone,
+          isActive: newIsActive
+        };
+      });
+
+      const action = clientAdminUpdated.isActive ? 'activated' : 'deactivated';
+
+      this.logger.log(`Client Admin with id: ${id} has been ${action}.`);
+      return clientAdminUpdated as ClientPayload;
+    } catch (error) {
+      this.logger.error('Error toggling activation for Client Admin with id: ${id}', error.stack);
+      handleException(error, 'Error toggling activation for Client Admin');
     }
   }
 }
