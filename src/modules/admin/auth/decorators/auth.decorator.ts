1a212a2675b23eab790be419997f6512f83fad93
Acide <143274747+AcideTI@users.noreply.github.com>
2024-09-20 17:25:17 -0500

    feat: agregamos el nuevo guard de refresh token

--- src/modules/admin/auth/decorators/auth.decorator.ts
+++ src/modules/admin/auth/decorators/auth.decorator.ts
@@ -1,7 +1,12 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { MustChangePasswordGuard } from '../guards/must-change-password-guards.guard';
+import { TokenRefresh } from '../guards/token-refresh.guard';
 
 export function Auth() {
-  return applyDecorators(UseGuards(AuthGuard('jwt')), UseGuards(MustChangePasswordGuard));
+  return applyDecorators(
+    UseGuards(AuthGuard('jwt')),
+    UseGuards(MustChangePasswordGuard),
+    UseGuards(TokenRefresh)
+  );
 }
