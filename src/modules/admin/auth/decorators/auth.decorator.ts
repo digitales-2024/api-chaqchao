1b3b23ab34925d49961c44039d4b7df0b4a93e46
EleoRomario <eleo.romario@gmail.com>
2024-09-20 22:21:31 -0500

    feat: actualizar decorador de autenticación para eliminar el guard de refresco de token

--- src/modules/admin/auth/decorators/auth.decorator.ts
+++ src/modules/admin/auth/decorators/auth.decorator.ts
@@ -1,12 +1,7 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { MustChangePasswordGuard } from '../guards/must-change-password-guards.guard';
-import { TokenRefresh } from '../guards/token-refresh.guard';
 
 export function Auth() {
-  return applyDecorators(
-    UseGuards(AuthGuard('jwt')),
-    UseGuards(MustChangePasswordGuard),
-    UseGuards(TokenRefresh)
-  );
+  return applyDecorators(UseGuards(AuthGuard('jwt')), UseGuards(MustChangePasswordGuard));
 }
