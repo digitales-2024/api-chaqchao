1a305af5196630d638740e461250bc1867fd8525
Alexander Cayro <alexandercayromamani@gmail.com>
2024-09-23 10:55:12 -0500

    feat: añadi las variaciones del producto en la vista de car item

--- src/interfaces/cart-item.interface.ts
+++ src/interfaces/cart-item.interface.ts
@@ -1,6 +1,7 @@
 import { CartItem } from '@prisma/client';
+import { ProductVariationData } from './product-variation.interface';
 
 export type CartItemData = Pick<CartItem, 'id' | 'cartId' | 'productId' | 'quantity' | 'price'> & {
   cart: { id: string; cartStatus: string };
-  product: { id: string; name: string; price: number };
+  product: { id: string; name: string; price: number; productVariations: ProductVariationData[] };
 };
