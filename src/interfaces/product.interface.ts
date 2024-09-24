495284aeb39693c59caf3a0cedb13fe2378d8af9
Diego Rondón <furrtr4532@gmail.com>
2024-09-24 14:50:03 -0500

    feat: agregar el isActive a la productData

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -3,7 +3,7 @@ import { ProductVariationData } from './product-variation.interface';
 
 export type ProductData = Pick<
   Product,
-  'id' | 'name' | 'description' | 'price' | 'image' | 'isAvailable'
+  'id' | 'name' | 'description' | 'price' | 'image' | 'isAvailable' | 'isActive'
 > & {
   category: { id: string; name: string };
   variations: ProductVariationData[];
