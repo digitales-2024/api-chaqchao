b1d0baef16a1f9e78a43ecb8541f40b95b785c1d
Acide <143274747+AcideTI@users.noreply.github.com>
2024-09-24 16:31:12 -0500

    feat: actualizar el DTO para crear un rol

--- src/modules/admin/rol/dto/create-rol.dto.ts
+++ src/modules/admin/rol/dto/create-rol.dto.ts
@@ -3,19 +3,13 @@ import { Transform } from 'class-transformer';
 import { IsArray, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateRolDto {
-  @ApiProperty({
-    description: 'Name of the rol',
-    example: 'admin'
-  })
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @Transform(({ value }) => value.trim().toLowerCase())
   name: string;
 
-  @ApiProperty({
-    description: 'Description of the rol',
-    example: 'Administrator'
-  })
+  @ApiProperty()
   @IsString()
   @Transform(({ value }) => value.trim().toLowerCase())
   description?: string;
@@ -23,5 +17,5 @@ export class CreateRolDto {
   @ApiProperty()
   @IsArray()
   @IsNotEmpty()
-  modulePermissions: string[];
+  rolPermissions: string[];
 }
