c2453205f455326402a4d47ebd3781737dcd3f05
EleoRomario <eleo.romario@gmail.com>
2024-09-30 13:09:56 -0500

    feat: agregar endpoint de ping en el controlador de la aplicación

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Version } from '@nestjs/common';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @Get('ping')
+  @Version('1')
+  ping(): string {
+    return 'Pong!';
+  }
+}
