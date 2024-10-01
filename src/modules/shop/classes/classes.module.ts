7514e0375cd31b116d513ad4900c15482c3c6dda
Diego Rondón <furrtr4532@gmail.com>
2024-10-01 11:16:25 -0500

    feat: agregar ClassScheduleModule a ClassesModule

--- src/modules/shop/classes/classes.module.ts
+++ src/modules/shop/classes/classes.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ClassesService } from './classes.service';
 import { ClassesController } from './classes.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ClassScheduleModule } from 'src/modules/admin/class-schedule/class-schedule.module';
 
 @Module({
   controllers: [ClassesController],
   providers: [ClassesService],
-  imports: [PrismaModule]
+  imports: [PrismaModule, ClassScheduleModule]
 })
 export class ClassesModule {}
