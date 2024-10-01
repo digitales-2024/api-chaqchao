983bfaf475ea61064f654bd52db7a883f5c13db3
Diego Rondón <furrtr4532@gmail.com>
2024-10-01 09:49:23 -0500

    feat: agregar DTO para la creación de clases con validaciones y transformaciones

---
+++ src/modules/shop/classes/dto/create-class.dto.ts
@@ -0,0 +1,60 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsDate, IsEmail, IsNumber, IsPhoneNumber, IsString } from 'class-validator';
+
+export class CreateClassDto {
+  @ApiProperty({
+    name: 'userName',
+    description: 'User name'
+  })
+  @IsString()
+  @Transform(({ value }) => value.trim().toLowerCase())
+  userName: string;
+
+  @ApiProperty({
+    name: 'userEmail',
+    description: 'User email'
+  })
+  @IsEmail()
+  @Transform(({ value }) => value.trim())
+  userEmail: string;
+
+  @ApiProperty({
+    name: 'userPhone',
+    description: 'User phone'
+  })
+  @IsPhoneNumber(null)
+  @Transform(({ value }) => value.trim())
+  userPhone: string;
+
+  @ApiProperty({
+    name: 'scheduleClass',
+    description: 'Class schedule'
+  })
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  scheduleClass: string;
+
+  @ApiProperty({
+    name: 'dateClass',
+    description: 'Class date'
+  })
+  @IsDate()
+  @Transform(({ value }) => value.trim())
+  dateClass: string;
+
+  @ApiProperty({
+    name: 'totalParticipants',
+    description: 'Total participants'
+  })
+  @IsNumber()
+  totalParticipants: number;
+
+  @ApiProperty({
+    name: 'comments',
+    description: 'Comments'
+  })
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  comments?: string;
+}
