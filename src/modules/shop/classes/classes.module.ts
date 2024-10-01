4f006afafd2916f15a05710417ba5b18932b352e
Diego Rondón <furrtr4532@gmail.com>
2024-10-01 12:06:55 -0500

    feat: agregar ClassLanguageModule a las importaciones de ClassesModule

--- src/modules/shop/classes/classes.module.ts
+++ src/modules/shop/classes/classes.module.ts
@@ -4,10 +4,11 @@ import { ClassesController } from './classes.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ClassScheduleModule } from 'src/modules/admin/class-schedule/class-schedule.module';
 import { ClassRegistrationModule } from 'src/modules/admin/class-registration/class-registration.module';
+import { ClassLanguageModule } from 'src/modules/admin/class-language/class-language.module';
 
 @Module({
   controllers: [ClassesController],
   providers: [ClassesService],
-  imports: [PrismaModule, ClassScheduleModule, ClassRegistrationModule]
+  imports: [PrismaModule, ClassScheduleModule, ClassRegistrationModule, ClassLanguageModule]
 })
 export class ClassesModule {}
