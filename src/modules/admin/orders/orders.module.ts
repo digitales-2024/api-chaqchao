912880c0986f533a9464a2995d3e726c70d179b4
EleoRomario <eleo.romario@gmail.com>
2024-10-02 15:12:16 -0500

    feat: agregar OrdersGateway al módulo de admin

--- src/modules/admin/orders/orders.module.ts
+++ src/modules/admin/orders/orders.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { OrdersGateway } from './orders.gateway';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersGateway],
   imports: [PrismaModule]
 })
 export class OrdersModule {}
