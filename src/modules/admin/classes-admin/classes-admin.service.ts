42e968532e7f506f4cf5ffa03ecceced5a635e99
Diego Rondón <furrtr4532@gmail.com>
2024-10-02 12:53:30 -0500

    feat: agregar método para obtener clases por fecha en el servicio ClassesAdmin

--- src/modules/admin/classes-admin/classes-admin.service.ts
+++ src/modules/admin/classes-admin/classes-admin.service.ts
@@ -56,8 +56,55 @@ export class ClassesAdminService {
     }
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} classesAdmin`;
+  /**
+   * Mostrar todos los registros de clases por fecha
+   * @param date Fecha de la clase
+   * @returns Registros de clases por fecha
+   */
+  async findByDate(date: string): Promise<ClassesData[]> {
+    try {
+      const classesRegistrations = await this.prisma.classes.findMany({
+        where: {
+          dateClass: new Date(date)
+        },
+        select: {
+          id: true,
+          userName: true,
+          userEmail: true,
+          userPhone: true,
+          totalParticipants: true,
+          totalAdults: true,
+          totalChildren: true,
+          totalPrice: true,
+          totalPriceAdults: true,
+          totalPriceChildren: true,
+          languageClass: true,
+          typeCurrency: true
+        },
+        orderBy: {
+          createdAt: 'asc'
+        }
+      });
+
+      // Mapea los resultados al tipo ClassesData
+      return classesRegistrations.map((classItem) => ({
+        id: classItem.id,
+        userName: classItem.userName,
+        userEmail: classItem.userEmail,
+        userPhone: classItem.userPhone,
+        totalParticipants: classItem.totalParticipants,
+        totalAdults: classItem.totalAdults,
+        totalChildren: classItem.totalChildren,
+        totalPrice: classItem.totalPrice,
+        totalPriceAdults: classItem.totalPriceAdults,
+        totalPriceChildren: classItem.totalPriceChildren,
+        languageClass: classItem.languageClass,
+        typeCurrency: classItem.typeCurrency
+      })) as ClassesData[];
+    } catch (error) {
+      this.logger.error('Error getting all products');
+      handleException(error, 'Error getting all products');
+    }
   }
 
   remove(id: string) {
