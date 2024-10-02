715e4bd232d334e7982b8184e85513f8d34c1218
Diego Rondón <furrtr4532@gmail.com>
2024-10-02 12:33:02 -0500

    feat: agregar servicio para el módulo ClassesAdmin que permite obtener todas las clases registradas

---
+++ src/modules/admin/classes-admin/classes-admin.service.ts
@@ -0,0 +1,66 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ClassesData } from 'src/interfaces';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { handleException } from 'src/utils';
+
+@Injectable()
+export class ClassesAdminService {
+  private readonly logger = new Logger(ClassesAdminService.name);
+
+  constructor(private readonly prisma: PrismaService) {}
+
+  /**
+   * Mostar todas las clases registradass
+   * @returns Clases registradas
+   */
+  async findAll(): Promise<ClassesData[]> {
+    try {
+      const classesRegistrations = await this.prisma.classes.findMany({
+        select: {
+          id: true,
+          userName: true,
+          userEmail: true,
+          userPhone: true,
+          totalParticipants: true,
+          totalAdults: true,
+          totalChildren: true,
+          totalPrice: true,
+          totalPriceAdults: true,
+          totalPriceChildren: true,
+          languageClass: true,
+          typeCurrency: true
+        },
+        orderBy: {
+          createdAt: 'asc'
+        }
+      });
+
+      // Mapea los resultados al tipo ClassesData
+      return classesRegistrations.map((classItem) => ({
+        id: classItem.id,
+        userName: classItem.userName,
+        userEmail: classItem.userEmail,
+        userPhone: classItem.userPhone,
+        totalParticipants: classItem.totalParticipants,
+        totalAdults: classItem.totalAdults,
+        totalChildren: classItem.totalChildren,
+        totalPrice: classItem.totalPrice,
+        totalPriceAdults: classItem.totalPriceAdults,
+        totalPriceChildren: classItem.totalPriceChildren,
+        languageClass: classItem.languageClass,
+        typeCurrency: classItem.typeCurrency
+      })) as ClassesData[];
+    } catch (error) {
+      this.logger.error('Error getting all products');
+      handleException(error, 'Error getting all products');
+    }
+  }
+
+  findOne(id: string) {
+    return `This action returns a #${id} classesAdmin`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} classesAdmin`;
+  }
+}
