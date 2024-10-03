0c91756d542011545b933f425ec2e1c7718bfb6a
EleoRomario <eleo.romario@gmail.com>
2024-10-03 16:52:19 -0500

    feat: agregar endpoint para actualizar el estado de un pedido

--- src/modules/admin/orders/orders.controller.ts
+++ src/modules/admin/orders/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { Auth } from '../auth/decorators';
 import { OrderStatus } from '@prisma/client';
@@ -20,5 +20,10 @@ export class OrdersController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.ordersService.findOne(id);
+  }
+
+  @Patch(':id')
+  updateStatus(@Param('id') id: string, @Body('status') status: OrderStatus) {
+    return this.ordersService.updateStatus(id, status);
   }
 }
