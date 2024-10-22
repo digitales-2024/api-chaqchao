c36467d4ef2cb0ccbfff42694130a1f996c83e29
EleoRomario <eleo.romario@gmail.com>
2024-10-22 09:44:22 -0500

    refactor: actualizar comando "build" para utilizar la ruta completa a "nest build"

--- prepare.ts
+++
@@ -1,6 +0,0 @@
-// scripts/prepare.js
-if (process.env.NODE_ENV !== 'production') {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { execSync } = require('child_process');
-  execSync('husky install', { stdio: 'inherit' });
-}
