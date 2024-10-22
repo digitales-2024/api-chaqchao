0c62c5ee10b3beb6a71facfc663a572ee6c160c1
EleoRomario <eleo.romario@gmail.com>
2024-10-22 09:26:13 -0500

    refactor: actualizar comando "prepare" para instalar Husky solo en entornos de desarrollo

--- prepare.ts
+++ prepare.ts
@@ -1,5 +1,6 @@
-import { execSync } from 'child_process';
 // scripts/prepare.js
 if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { execSync } = require('child_process');
   execSync('husky install', { stdio: 'inherit' });
 }
