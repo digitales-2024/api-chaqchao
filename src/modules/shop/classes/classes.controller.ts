add1110ba0b650f873661d05276c432c7261533d
EleoRomario <eleo.romario@gmail.com>
2024-10-23 16:58:18 -0500

    feat: añadir endpoints para obtener todas las clases y todos los idiomas de clase

--- src/modules/shop/classes/classes.controller.ts
+++ src/modules/shop/classes/classes.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { ClassesService } from './classes.service';
 import { CreateClassDto } from './dto/create-class.dto';
 import {
@@ -7,7 +7,9 @@ import {
   ApiInternalServerErrorResponse,
   ApiTags
 } from '@nestjs/swagger';
-import { ClassesData, HttpResponse } from 'src/interfaces';
+import { ClassesData, ClassLanguageData, ClassScheduleData, HttpResponse } from 'src/interfaces';
+import { ClassScheduleService } from 'src/modules/admin/class-schedule/class-schedule.service';
+import { ClassLanguageService } from 'src/modules/admin/class-language/class-language.service';
 
 @ApiTags('Classes')
 @ApiInternalServerErrorResponse({ description: 'Internal server error' })
@@ -17,11 +19,27 @@ import { ClassesData, HttpResponse } from 'src/interfaces';
   version: '1'
 })
 export class ClassesController {
-  constructor(private readonly classesService: ClassesService) {}
+  constructor(
+    private readonly classesService: ClassesService,
+    private readonly classScheduleService: ClassScheduleService,
+    private readonly classLanguageService: ClassLanguageService
+  ) {}
 
   @ApiCreatedResponse({ description: 'Class created' })
   @Post()
   create(@Body() createClassDto: CreateClassDto): Promise<HttpResponse<ClassesData>> {
     return this.classesService.create(createClassDto);
+  }
+
+  @ApiBadRequestResponse({ description: 'Not found class' })
+  @Get()
+  findAll(): Promise<ClassScheduleData[]> {
+    return this.classScheduleService.findAll();
+  }
+
+  @ApiBadRequestResponse({ description: 'Not found language class' })
+  @Get('/languages')
+  findAllLanguages(): Promise<ClassLanguageData[]> {
+    return this.classLanguageService.findAll();
   }
 }
