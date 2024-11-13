11447ad9051aca52cc3e305f0389d1e97e914c88
EleoRomario <eleo.romario@gmail.com>
2024-11-13 12:13:17 -0500

    feat: agregar AdminGateway como proveedor en el módulo de pedidos

--- src/modules/shop/order/order.module.ts
+++ src/modules/shop/order/order.module.ts
@@ -4,10 +4,11 @@ import { OrderService } from './order.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CartModule } from '../cart/cart.module';
 import { OrdersService } from 'src/modules/admin/orders/orders.service';
+import { AdminGateway } from 'src/modules/admin/admin.gateway';
 
 @Module({
   controllers: [OrderController],
-  providers: [OrderService, OrdersService],
+  providers: [OrderService, OrdersService, AdminGateway],
   imports: [PrismaModule, forwardRef(() => CartModule)],
   exports: [OrderService]
 })
