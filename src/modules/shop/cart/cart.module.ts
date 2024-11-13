259cd8fe64c294cc1a1b221a87fb04f249a29831
EleoRomario <eleo.romario@gmail.com>
2024-11-13 12:12:41 -0500

    feat: refactor CartController para la gestión de carritos y agregar nuevas funcionalidades

--- src/modules/shop/cart/cart.module.ts
+++ src/modules/shop/cart/cart.module.ts
@@ -1,14 +1,14 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CartController } from './cart.controller';
 import { CartService } from './cart.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { ClientModule } from '../client/client.module';
 import { PickupCodeService } from './pickup-code/pickup-code.service';
+import { AdminGateway } from 'src/modules/admin/admin.gateway';
 
 @Module({
   controllers: [CartController],
-  providers: [CartService, PickupCodeService],
-  imports: [PrismaModule, forwardRef(() => ClientModule)],
+  providers: [CartService, PickupCodeService, AdminGateway],
+  imports: [PrismaModule],
   exports: [CartService]
 })
 export class CartModule {}
