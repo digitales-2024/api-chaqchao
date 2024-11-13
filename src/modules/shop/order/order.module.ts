9a6b71015c705d5426fe301ec2e4342ec25d9609
EleoRomario <eleo.romario@gmail.com>
2024-11-13 11:37:22 -0500

    feat: eliminar CreateOrderDto y su uso en OrderController y OrderService

--- src/modules/shop/order/order.module.ts
+++ src/modules/shop/order/order.module.ts
@@ -3,12 +3,11 @@ import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CartModule } from '../cart/cart.module';
-import { AdminGateway } from 'src/modules/admin/admin.gateway';
 import { OrdersService } from 'src/modules/admin/orders/orders.service';
 
 @Module({
   controllers: [OrderController],
-  providers: [OrderService, OrdersService, AdminGateway],
+  providers: [OrderService, OrdersService],
   imports: [PrismaModule, forwardRef(() => CartModule)],
   exports: [OrderService]
 })
