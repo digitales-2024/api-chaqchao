ce29870ab8d99e71c86adb2caa1f722f77e35e50
EleoRomario <eleo.romario@gmail.com>
2024-11-14 14:49:04 -0500

    feat: modificar decorador GetClient para retornar null si no se encuentra el cliente

--- src/modules/shop/auth/decorators/get-client.decorator.ts
+++ src/modules/shop/auth/decorators/get-client.decorator.ts
@@ -1,16 +1,11 @@
-import {
-  createParamDecorator,
-  ExecutionContext,
-  InternalServerErrorException
-} from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { ClientData } from 'src/interfaces';
 
 export const GetClient = createParamDecorator((data, ctx: ExecutionContext): ClientData => {
   const request = ctx.switchToHttp().getRequest();
   const client = request.user;
-
   if (!client) {
-    throw new InternalServerErrorException('Client not found');
+    return null;
   }
 
   return {
