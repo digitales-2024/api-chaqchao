6c220f2c6b0e811c2805fd3648ec190859a337b3
EleoRomario <eleo.romario@gmail.com>
2024-11-14 11:02:04 -0500

    feat: actualizar CreateCartDto para incluir tempId y eliminar cartStatus

--- src/modules/shop/cart/dto/create-cart.dto.ts
+++ src/modules/shop/cart/dto/create-cart.dto.ts
@@ -1,14 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { CartStatus } from '@prisma/client';
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsOptional } from 'class-validator';
 export class CreateCartDto {
+  cartStatus?: CartStatus;
+
   @ApiProperty({
-    description: 'Estado del carrito',
-    enum: CartStatus,
-    default: CartStatus.PENDING,
+    description: 'ID temporal del carrito',
     required: false
   })
   @IsOptional()
-  @IsEnum(CartStatus)
-  cartStatus?: CartStatus;
+  tempId?: string;
 }
