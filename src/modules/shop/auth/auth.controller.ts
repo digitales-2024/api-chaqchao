7001efb7fe8bc0be9fbb8bc16d02bbdc8d2f47b9
EleoRomario <eleo.romario@gmail.com>
2024-11-19 09:18:37 -0500

    feat: mejorar la documentación de las operaciones de auth controller

--- src/modules/shop/auth/auth.controller.ts
+++ src/modules/shop/auth/auth.controller.ts
@@ -12,7 +12,6 @@ import {
 import { Response, Request } from 'express';
 import { AuthService } from './auth.service';
 import { GoogleAuthGuard } from './guards/google-guards.guard';
-import { AuthGuard } from '@nestjs/passport';
 import { GetClient } from './decorators/get-client.decorator';
 import { ClientData, HttpResponse } from 'src/interfaces';
 import {
@@ -21,6 +20,7 @@ import {
   ApiInternalServerErrorResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
+  ApiOperation,
   ApiTags,
   ApiUnauthorizedResponse
 } from '@nestjs/swagger';
@@ -32,7 +32,7 @@ import { ResetPasswordClientDto } from './dto/reset-password-client.dto';
 import { ClientAuth } from './decorators/client-auth.decorator';
 import { ClientRefreshAuth } from './decorators/client-refresh-auth.decorator';
 
-@ApiTags('Auth Client')
+@ApiTags('Shop Auth')
 @ApiInternalServerErrorResponse({ description: 'Internal server error' })
 @Controller({ path: 'auth/client', version: '1' })
 export class AuthController {
@@ -41,12 +41,20 @@ export class AuthController {
     private readonly configService: ConfigService
   ) {}
 
-  @ApiOkResponse({ description: 'Google login redirect' })
+  /**
+   * Redirigir al usuario a la página de inicio de sesión de Google.
+   */
+
   @Get('google/login')
+  @ApiOperation({ summary: 'Redirigir al usuario a la página de inicio de sesión de Google' })
   @UseGuards(GoogleAuthGuard)
   handleLogin() {}
 
+  /**
+   * Manejar la redirección de Google.
+   */
   @Get('google/redirect')
+  @ApiOperation({ summary: 'Manejar la redirección de Google' })
   @UseGuards(GoogleAuthGuard)
   async handleRedirect(@Req() req: Request, @Res() res: Response) {
     const webUrlShop = this.configService.get<string>('WEB_URL_SHOP');
@@ -69,47 +77,66 @@ export class AuthController {
     }
   }
 
-  @ApiBadRequestResponse({ description: 'Bad request' })
-  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
-  @ClientAuth()
-  @ApiOkResponse({ description: 'Client profile' })
+  /**
+   * Mostrar el perfil del cliente.
+   */
   @Get('profile')
-  @UseGuards(AuthGuard('client-jwt'))
+  @ApiOperation({ summary: 'Mostrar el perfil del cliente' })
+  @ApiBadRequestResponse({ description: 'Ocurrio un error al mostrar el perfil del cliente' })
+  @ApiUnauthorizedResponse({ description: 'No autorizado' })
+  @ApiOkResponse({ description: 'Perfil del cliente' })
+  @ClientAuth()
   getClientProfile(@GetClient() client: ClientData) {
     return client;
   }
 
-  @ApiOkResponse({ description: 'Client logged in successfully' })
-  @ApiBadRequestResponse({ description: 'Bad request' })
-  @ApiNotFoundResponse({ description: 'Client not found' })
+  /**
+   * Iniciar sesión del cliente.
+   */
   @Post('login')
+  @ApiOperation({ summary: 'Iniciar sesión del cliente' })
+  @ApiOkResponse({ description: 'El cliente inició sesión correctamente' })
+  @ApiBadRequestResponse({ description: 'Solicitud incorrecta ' })
+  @ApiNotFoundResponse({ description: 'Cliente no encontrada' })
   async login(@Body() loginAuthClientDto: LoginAuthClientDto, @Res() res: Response): Promise<void> {
     const clientDataLogin = await this.authService.login(loginAuthClientDto, res);
     res.status(HttpStatus.OK).json(clientDataLogin);
   }
 
-  @ApiBadRequestResponse({ description: 'Bad request' })
-  @ApiOkResponse({ description: 'Client successfully registered' })
+  /**
+   * Registrar un nuevo cliente.
+   */
   @Post('register')
+  @ApiOperation({ summary: 'Registrar un nuevo cliente' })
+  @ApiBadRequestResponse({ description: 'Solicitud incorrecta' })
+  @ApiOkResponse({ description: 'Cliente registrado correctamente' })
   async register(@Body() createClientDto: CreateClientDto, @Res() res: Response): Promise<void> {
     const clientDataRegister = await this.authService.create(createClientDto, res);
     res.status(HttpStatus.OK).json(clientDataRegister);
   }
 
-  @ApiBadRequestResponse({ description: 'Bad request' })
-  @ApiNotFoundResponse({ description: 'Client not found' })
-  @ApiOkResponse({ description: 'Email sent successfully' })
+  /**
+   * Enviar un correo electrónico para restablecer la contraseña.
+   */
   @Post('forgot-password')
+  @ApiOperation({ summary: 'Enviar un correo electrónico para restablecer la contraseña' })
+  @ApiBadRequestResponse({ description: 'Solicitud incorrecta' })
+  @ApiNotFoundResponse({ description: 'Cliente no encontrada' })
+  @ApiOkResponse({ description: 'Correo electrónico enviado correctamente' })
   async forgotPassword(
     @Body() forgotPasswordClientDto: ForgotPasswordClientDto
   ): Promise<HttpResponse<string>> {
     return this.authService.forgotPassword(forgotPasswordClientDto);
   }
 
-  @ApiBadRequestResponse({ description: 'Bad request' })
-  @ApiNotFoundResponse({ description: 'Client not found' })
-  @ApiOkResponse({ description: 'Password reset successfully' })
+  /**
+   * Restablecer la contraseña del cliente.
+   */
   @Post('reset-password')
+  @ApiOperation({ summary: 'Restablecer la contraseña del cliente' })
+  @ApiBadRequestResponse({ description: 'Solicitud incorrecta' })
+  @ApiNotFoundResponse({ description: 'Cliente no encontrada' })
+  @ApiOkResponse({ description: 'Restablecer la contraseña correctamente' })
   async resetPassword(
     @Query('token') token: string,
     @Body() resetPasswordClientDto: ResetPasswordClientDto
@@ -117,17 +144,25 @@ export class AuthController {
     return this.authService.resetPassword(token, resetPasswordClientDto);
   }
 
-  @ClientAuth()
-  @ApiBadRequestResponse({ description: 'Bad request' })
-  @ApiOkResponse({ description: 'Logout client' })
+  /**
+   * Cerrar sesión del cliente.
+   */
   @Get('logout')
+  @ApiOperation({ summary: 'Cerrar sesión del cliente' })
+  @ApiBadRequestResponse({ description: 'Solicitud incorrecta' })
+  @ApiOkResponse({ description: 'Se cerró la sesión correctamente' })
+  @ClientAuth()
   async logout(@Res() res: Response): Promise<void> {
     return this.authService.logout(res);
   }
 
-  @ApiCreatedResponse({ description: 'Refresh token' })
-  @ClientRefreshAuth()
+  /**
+   * Refrescar el token de autenticación.
+   */
   @Post('refresh-token')
+  @ApiOperation({ summary: 'Refrescar el token de autenticación' })
+  @ApiCreatedResponse({ description: 'Actualización del token' })
+  @ClientRefreshAuth()
   async refreshToken(@Req() req: Request, @Res() res: Response): Promise<void> {
     return this.authService.refreshToken(req, res);
   }
