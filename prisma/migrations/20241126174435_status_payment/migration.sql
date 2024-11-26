d3c11b95b458d5978b007c86eaf66fb13011affa
EleoRomario <eleo.romario@gmail.com>
2024-11-26 15:31:58 -0500

    feat: actualizar enum PaymentStatus y agregar columna businessName en BillingDocument

---
+++ prisma/migrations/20241126174435_status_payment/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [PENDING,FAILED] on the enum `PaymentStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "PaymentStatus_new" AS ENUM ('PAID', 'RUNNING', 'UNPAID', 'ABANDONED');
+ALTER TABLE "BillingDocument" ALTER COLUMN "paymentStatus" TYPE "PaymentStatus_new" USING ("paymentStatus"::text::"PaymentStatus_new");
+ALTER TYPE "PaymentStatus" RENAME TO "PaymentStatus_old";
+ALTER TYPE "PaymentStatus_new" RENAME TO "PaymentStatus";
+DROP TYPE "PaymentStatus_old";
+COMMIT;
