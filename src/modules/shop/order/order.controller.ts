e90d8b94c077cddc8241b9298e01c5f09f9837b7
EleoRomario <eleo.romario@gmail.com>
2024-12-06 16:36:29 -0500

    feat: agregar funcionalidad para exportar pedidos en formato PDF
    Mejorar la obtención de detalles de pedidos

--- src/modules/shop/order/order.controller.ts
+++ src/modules/shop/order/order.controller.ts
@@ -5,13 +5,14 @@ import {
   ApiTags,
   ApiUnauthorizedResponse
 } from '@nestjs/swagger';
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { ClientAuth } from '../auth/decorators/client-auth.decorator';
 import { ClientPayload } from 'src/interfaces';
 import { GetClient } from '../auth/decorators/get-client.decorator';
+import { Response } from 'express';
 
-@ApiTags('Order')
+@ApiTags('Shop Order')
 @ApiUnauthorizedResponse({ description: 'Unauthorized' })
 @ApiInternalServerErrorResponse({ description: 'Internal server error' })
 @ApiBadRequestResponse({ description: 'Bad Request' })
@@ -23,15 +24,36 @@ import { GetClient } from '../auth/decorators/get-client.decorator';
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
-  @ApiOkResponse({ description: 'Get paid order information' })
+  /**
+   * Mostrar detalle de una orden de un usuario.
+   */
   @Get('details/:id')
+  @ApiOkResponse({ description: 'Get paid order information' })
+  @ClientAuth()
   async getOrderDetails(@Param('id') id: string) {
     return this.orderService.getOrderDetails(id);
   }
 
-  @ApiOkResponse({ description: 'Orders client' })
+  /**
+   * Recupera los pedidos para una cliente.
+   */
   @Get('/orders')
+  @ApiOkResponse({ description: 'Orders client' })
+  @ClientAuth()
   async getOrders(@GetClient() client: ClientPayload) {
     return await this.orderService.getOrders(client.id);
+  }
+
+  /**
+   * Exportar un pedido en formato PDF
+   */
+  @Post('export/pdf/:id')
+  @ClientAuth()
+  async exportPdf(@Res() res: Response, @Param('id') id: string) {
+    const { code, pdfBuffer } = await this.orderService.exportPdfOrder(id);
+    // Enviar el archivo PDF en la respuesta
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', `attachment; filename="Pedido-${code}.pdf"`);
+    res.send(pdfBuffer);
   }
 }
