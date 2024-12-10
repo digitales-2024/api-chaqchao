ea3323abf0a14dfa8ee91b3d6937fa3be4ca4b3b
EleoRomario <eleo.romario@gmail.com>
2024-12-10 12:38:02 -0500

    feat: mejorar la documentación y las respuestas de la API en el controlador de artículos del carrito

--- src/modules/shop/cart-item/cart-item.controller.ts
+++ src/modules/shop/cart-item/cart-item.controller.ts
@@ -10,10 +10,13 @@ import {
   ApiCreatedResponse,
   ApiTags,
   ApiUnauthorizedResponse,
-  ApiOkResponse
+  ApiOkResponse,
+  ApiBody,
+  ApiOperation,
+  ApiParam
 } from '@nestjs/swagger';
 
-@ApiTags('CartItem')
+@ApiTags('Shop Cart')
 @ApiBadRequestResponse({ description: 'Bad Request' })
 @ApiUnauthorizedResponse({ description: 'UnAuthorized' })
 @Auth()
@@ -24,26 +27,60 @@ import {
 export class CartItemController {
   constructor(private readonly cartItemService: CartItemService) {}
 
-  @ApiCreatedResponse({ description: 'Cart Item Created' })
+  /**
+   * Crear un item en el carrito de compras
+   * @summary Crear un item en el carrito de compras
+   * @param createCartItemDto Datos del item para el carrito de compras
+   * @returns Item del carrito de compras creado
+   */
   @Post()
+  @ApiOperation({ summary: 'Crear un item en el carrito de compras' })
+  @ApiCreatedResponse({ description: 'Artículo del carrito creado' })
+  @ApiBody({ type: CreateCartItemDto, description: 'Datos del artículo para el carrito' })
   create(@Body() createCartItemDto: CreateCartItemDto): Promise<HttpResponse<CartItemData>> {
     return this.cartItemService.create(createCartItemDto);
   }
 
-  @ApiOkResponse({ description: 'Get all carts item' })
+  /**
+   * Recupere todos los artículos en el carrito de compras.
+   * @returns Una promesa que resuelve una matriz de todos los artículos del carrito.
+   */
   @Get()
+  @ApiOperation({ summary: 'Recupere todos los artículos en el carrito de compras' })
+  @ApiOkResponse({ description: 'Artículos del carrito recuperados' })
   findAll(): Promise<CartItemData[]> {
     return this.cartItemService.findAll();
   }
 
-  @ApiOkResponse({ description: 'Product deleted' })
+  /**
+   * Elimina un item del carrito de compras.
+   * @summary Elimina un item del carrito de compras.
+   * @param id Identificador del item del carrito.
+   * @returns Mensaje de confirmación de eliminación.
+   */
   @Delete(':id')
+  @ApiOperation({ summary: 'Elimina un item del carrito de compras' })
+  @ApiOkResponse({ description: 'Artículo del carrito eliminado' })
+  @ApiParam({ name: 'id', description: 'Identificador del artículo del carrito' })
   remove(@Param('id') id: string): Promise<HttpResponse<CartItemData>> {
     return this.cartItemService.remove(id);
   }
 
-  @ApiOkResponse({ description: 'Cart item quantity updated' })
+  /**
+   * Actualizar la cantidad de un item en el carrito de compras.
+   * @summary Actualizar la cantidad de un item en el carrito de compras.
+   * @param id Identificador del item del carrito.
+   * @param updateCartItemDto Información para la actualización del item del carrito.
+   * @returns Promesa con los datos del item del carrito actualizado.
+   */
   @Patch(':id')
+  @ApiOperation({ summary: 'Actualiza la cantidad de un item en el carrito de compras' })
+  @ApiOkResponse({ description: 'Artículo del carrito actualizado' })
+  @ApiParam({ name: 'id', description: 'Identificador del artículo del carrito' })
+  @ApiBody({
+    type: UpdateQuantityCartItemDto,
+    description: 'Datos para la actualización del artículo del carrito'
+  })
   updateQuantity(
     @Param('id') id: string,
     @Body() updateCartItemDto: UpdateQuantityCartItemDto
