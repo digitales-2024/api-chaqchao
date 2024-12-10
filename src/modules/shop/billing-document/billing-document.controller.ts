4a4d0e9b75731acc84567fa637862f4c31cad688
EleoRomario <eleo.romario@gmail.com>
2024-12-10 12:49:29 -0500

    feat: mejorar la documentación y las respuestas de la API en el controlador de documentos de factura

--- src/modules/shop/billing-document/billing-document.controller.ts
+++ src/modules/shop/billing-document/billing-document.controller.ts
@@ -1,9 +1,12 @@
 import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
+  ApiBody,
   ApiCreatedResponse,
   ApiInternalServerErrorResponse,
   ApiOkResponse,
+  ApiOperation,
+  ApiParam,
   ApiTags,
   ApiUnauthorizedResponse
 } from '@nestjs/swagger';
@@ -14,7 +17,7 @@ import { CreateBillingDocumentDto } from './dto/create-billing-document.dto';
 import { HttpResponse } from 'src/interfaces';
 import { UpdateStatusBillingDocumentDto } from './dto/update-status-billing.dto';
 
-@ApiTags('BillingDocument')
+@ApiTags('Shop Billing')
 @ApiUnauthorizedResponse({ description: 'Unauthorized' })
 @ApiInternalServerErrorResponse({ description: 'Internal Server Error' })
 @ApiBadRequestResponse({ description: 'Bad Request' })
@@ -26,21 +29,57 @@ import { UpdateStatusBillingDocumentDto } from './dto/update-status-billing.dto'
 export class BillingDocumentController {
   constructor(private readonly billingDocumentService: BillingDocumentService) {}
 
-  @ApiOkResponse({ description: 'Get all billing documents' })
+  /**
+   * Recupera todos los documentos de facturación.
+   *
+   * @returns Una promesa que resuelve una variedad de BillingDocumentData
+   * que contiene los detalles de todos los documentos de facturación.
+   */
   @Get()
+  @ApiOperation({ summary: 'Obtenga todos los documentos de facturación' })
+  @ApiOkResponse({ description: 'Documentos de facturación recuperados' })
   findAll(): Promise<BillingDocumentData[]> {
     return this.billingDocumentService.findAll();
   }
 
-  @ApiCreatedResponse({ description: 'Billing Document created' })
+  /**
+   * Crea un nuevo documento de facturación.
+   *
+   * @param createBillingDocumentDto - Datos necesarios para crear un nuevo documento de facturación.
+   * @returns Una promesa que resuelve un HttpResponse que contiene los detalles del documento de facturación creado.
+   */
   @Post()
+  @ApiOperation({ summary: 'Crear un nuevo documento de facturación' })
+  @ApiCreatedResponse({ description: 'Documento de facturación creado' })
+  @ApiBody({
+    type: CreateBillingDocumentDto,
+    description: 'Datos necesarios para crear un nuevo documento de facturación'
+  })
   create(
     @Body() createBillingDocumentDto: CreateBillingDocumentDto
   ): Promise<HttpResponse<BillingDocumentData>> {
     return this.billingDocumentService.create(createBillingDocumentDto);
   }
-  @ApiOkResponse({ description: 'Billing Document Status updated' })
+
+  /**
+   * Actualiza el estado de un documento de facturación.
+   *
+   * @param id - Identificador del documento de facturación.
+   * @param updateStatusBillingDocumentDto - Contiene el nuevo estado del documento de facturación.
+   * @returns Una promesa que resuelve un HttpResponse que contiene los detalles del documento de facturación actualizado.
+   */
   @Patch(':id/status')
+  @ApiOperation({ summary: 'Actualizar el estado del documento de facturación' })
+  @ApiOkResponse({ description: 'Documento de facturación actualizado' })
+  @ApiBody({
+    type: UpdateStatusBillingDocumentDto,
+    description: 'Contiene el nuevo estado del documento de facturación'
+  })
+  @ApiParam({
+    name: 'id',
+    type: 'string',
+    description: 'Identificador del documento de facturación'
+  })
   async updateBillingDocumentStatus(
     @Param('id') id: string,
     @Body() updateStatusBillingDocumentDto: UpdateStatusBillingDocumentDto
