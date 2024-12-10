ddd91d18264d82c46a3df0d5ac98c84ce23ecfdf
EleoRomario <eleo.romario@gmail.com>
2024-12-10 11:34:17 -0500

    feat: actualizar la documentación y etiquetas en el controlador de registro de clases

--- src/modules/admin/class-registration/dto/create-class-registration.dto.ts
+++ src/modules/admin/class-registration/dto/create-class-registration.dto.ts
@@ -2,17 +2,27 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsUUID } from 'class-validator';
 
 export class CreateClassRegistrationDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Intervalo de tiempo en minutos para cerrar la clase antes de que comience',
+    example: 30
+  })
   @IsNumber()
   @IsNotEmpty()
   closeBeforeStartInterval: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'Intervalo de tiempo en minutos para cerrar el registro de la clase después de que comience',
+    example: 60
+  })
   @IsNumber()
   @IsNotEmpty()
   finalRegistrationCloseInterval: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Id del negocio',
+    example: '123e4567-e89b-12d3-a456-426614174000'
+  })
   @IsNotEmpty()
   @IsUUID()
   businessId: string;
