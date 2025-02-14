2a31abf2cb1d7a4003820fd615d45a268f553940
Camila <camila.rodriguezbrown@ucsm.edu.pe>
2025-02-14 13:22:21 -0500

    feat: agregar servicio de envíos interciudades y actualizar lógica de órdenes y reportes

--- src/modules/admin/orders/orders.service.ts
+++ src/modules/admin/orders/orders.service.ts
@@ -98,6 +98,7 @@ export class OrdersService {
         pickupTime: order.pickupTime,
         isActive: order.isActive,
         someonePickup: order.someonePickup,
+        isShipping: order.isShipping,
         pickupCode: order.pickupCode,
         totalAmount: order.totalAmount,
         client: order.cart.client
@@ -185,6 +186,7 @@ export class OrdersService {
         pickupTime: order.pickupTime,
         isActive: order.isActive,
         someonePickup: order.someonePickup,
+        isShipping: order.isShipping,
         pickupCode: order.pickupCode,
         totalAmount: order.totalAmount,
         cart: {
