c6e4381ed0eca208928aa33f67320877c5b3a45d
Camila <camila.rodriguezbrown@ucsm.edu.pe>
2025-02-20 15:00:37 -0500

    refactor: remove isShipping property from order interfaces and reports

--- src/interfaces/order.interface.ts
+++ src/interfaces/order.interface.ts
@@ -11,7 +11,6 @@ export type OrderData = Pick<
   | 'isActive'
   | 'cartId'
   | 'someonePickup'
-  | 'isShipping'
   | 'pickupCode'
   | 'totalAmount'
 > & {
@@ -25,7 +24,6 @@ export type OrderInfo = Pick<
   | 'pickupAddress'
   | 'pickupTime'
   | 'someonePickup'
-  | 'isShipping'
   | 'pickupCode'
   | 'totalAmount'
   | 'isActive'
