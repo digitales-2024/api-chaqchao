c2693ce8765845d970c5de3de0a0d850e4732b59
eleoromario <eleo.romario@gmail.com>
2025-02-26 15:15:33 -0500

    refactor: eliminar propiedad isShipping de las interfaces de orden y servicio

--- src/modules/admin/orders/orders.service.ts
+++ src/modules/admin/orders/orders.service.ts
@@ -98,7 +98,6 @@ export class OrdersService {
         pickupTime: order.pickupTime,
         isActive: order.isActive,
         someonePickup: order.someonePickup,
-        isShipping: order.isShipping,
         pickupCode: order.pickupCode,
         totalAmount: order.totalAmount,
         client: order.cart.client
@@ -186,7 +185,6 @@ export class OrdersService {
         pickupTime: order.pickupTime,
         isActive: order.isActive,
         someonePickup: order.someonePickup,
-        isShipping: order.isShipping,
         pickupCode: order.pickupCode,
         totalAmount: order.totalAmount,
         cart: {
