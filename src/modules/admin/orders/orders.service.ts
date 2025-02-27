a5c3b14ab30315c0bf13217297736035eceaf346
Acide <143274747+AcideTI@users.noreply.github.com>
2025-02-27 11:46:49 -0500

    Revert "Feature/chaq 9 servicio de envíos interciudades"

--- src/modules/admin/orders/orders.service.ts
+++ src/modules/admin/orders/orders.service.ts
@@ -171,6 +171,7 @@ export class OrdersService {
               state: true,
               country: true,
               city: true,
+              postalCode: true,
               typeDocument: true,
               businessName: true,
               paymentStatus: true
