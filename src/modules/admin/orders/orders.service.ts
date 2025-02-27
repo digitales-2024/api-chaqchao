a8257c740f40ecd4459af295bf31ad784c5510f1
eleoromario <eleo.romario@gmail.com>
2025-02-27 15:36:29 -0500

    refactor: remove postalCode from billing document interfaces and related services

--- src/modules/admin/orders/orders.service.ts
+++ src/modules/admin/orders/orders.service.ts
@@ -171,7 +171,6 @@ export class OrdersService {
               state: true,
               country: true,
               city: true,
-              postalCode: true,
               typeDocument: true,
               businessName: true,
               paymentStatus: true
